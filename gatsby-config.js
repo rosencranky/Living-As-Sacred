00bbb5758ca2face242462f88a01c72f751e535e
ronsencranky <rosencranky.png@gmail.com>
2023-06-21 17:14:43 +0100

    fix gatby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: `${process.env.DATO_API_TOKEN}`,
+        apiToken: `70e39355c82b76ef50c29d645aa5f`,
         preview: false,
         disableLiveReload: false,
         localeFallbacks: {
