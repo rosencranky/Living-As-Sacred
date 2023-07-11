f1a9ad7ee02d8fa65ae33fc174ba672cad5ca575
rosencranky <104981105+rosencranky@users.noreply.github.com>
2023-07-11 21:13:08 +0100

    update > remove PT version (#20)
    * create color palette in tailwind.config
    
    * start styling typography
    
    * update tailwind.config and globals.css to reflect new typography
    
    * restructure tailwind.config to reflect new typography
    
    * finish adjusting typography to new design
    
    * remove unecessary language links and adjust header margins
    
    * style about section
    
    * fix margins on about section button
    
    * adjust margins on quote section
    
    * style values section for desktop
    
    * adjust CTA section margins, adjust border radius for buttons
    
    * switch cta3 section background
    
    * style cta-move section for desktop
    
    * style cta-plants section for desktop
    
    * style price section for mobile
    
    * begin styling soul section
    
    * style soul section for desktop
    
    * style support section for desktop
    
    * small adjustments to margins
    
    * style accordion component for desktop
    
    * done:style offering section for desktop
    
    * add new splash image, style meet us section for desktop (#5)
    
    * Update: Style & Content > Timeline (#6)
    
    * ongoing: style timeline section
    
    * style timeline for desktop
    
    * ongoing: style timeline for desktop
    
    * fix: gatsby-config (#7)
    
    * ongoing: style timeline section
    
    * style timeline for desktop
    
    * ongoing: style timeline for desktop
    
    * fix gatby-config
    
    * test: apitoken
    
    * test: apitoken full
    
    * fix: deploy issue (#8)
    
    * update lock files to test deploy
    
    * rename cta3
    
    * change token in gatsby-config
    
    * style values section for responsiveness (#9)
    
    * style offering section for responsiveness (#10)
    
    * Update > Responsive > About & h4 elements (#11)
    
    * style about section, h4 elements and .beige-card class for mobile
    
    * fine tune h4 responsive line height
    
    * fix gap responsiveness in about section
    
    * fix responsive text in soul section
    
    * Update > Responsive > Timeline (#12)
    
    * begin styling timeline for mobile
    
    * style timeline for responsiveness
    
    * apply several touch-ups to responsive design (#13)
    
    * Update > Style > Pages (#14)
    
    * style terms and contact pages
    
    * fix mobile menu background issue
    
    * remove pt version, style 404 page
    
    * remove pt, update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,17 +2,18 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <div className="section h-90 flex justify-center">
-      <h2 className="text-5xl md:text-6xl xl:text-8xl text-center text-red absolute top-1/2">
-        Page not found :(
+    <div className="section h-90 flex justify-evenly   items-center flex-col">
+      <h2 className="text-5xl md:text-6xl xl:text-8xl text-center text-red ">
+        Page not found <br/> :(
       </h2>
-      {/* <h2 className="top-margin-medium text-center underline">
+      <h2 className="z-20 inline-block  border border-green rounded-2xl xl:rounded-4xl bg-khaki hover:bg-beige hover:border-khaki text-black px-5 py-2 monotext text-sm xl:text-2xl text-center no-underline lg:w-1/5 leading-none shadow-sm  transition ease-in-out duration-300">
         <Link to="/">Head home</Link>
-      </h2> */}
+      </h2>
     </div>
   </Layout>
 )
